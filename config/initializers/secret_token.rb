b1d80818f2dc340e365070ebc05588e4226874f8
Garenthino Marcough <garenthino@gmail.com>
2012-11-08 19:45:19 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Amazonscraper::Application.config.secret_token = '6e3df9a6b3a9fa1b1a32e78a9fd9ec53111c44375b6c32fb7f63925aa3a995325fc6b8f21454cca5008ccc1594918b3f8ab35f79b3639d9a5360c92b698842e8'
